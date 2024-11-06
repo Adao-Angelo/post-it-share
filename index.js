b9719e50275a5f4c8f498a32e36b57239a0edb12
Mayk Brito <maykbrito@gmail.com>
2024-11-05 21:13:54 -0300

    parte IIII HTMX

--- index.js
+++ index.js
@@ -14,10 +14,10 @@ app.get('/note/:id', (req, res) => {
 })
 
 app.post('/notes', async (req, res) => {
-  const { content } = req.body;
+  const { content } = req.body
 
   if (!content) {
-    return res.send('<span>Erro inesperado!</span>')
+    return res.send('<span class="error">Erro inesperado!</span>')
   }
 
   const id = crypto.randomUUID();
